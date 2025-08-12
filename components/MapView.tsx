018f9747629235f4c73677b1946349b8dec4991d
Dan Barrington <daniel.barrington@healthunlocked.com>
2025-08-12 22:19:29 +0100

    Migrate project to TypeScript

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,7 +1,11 @@
-export default function MapView() {
+import React from 'react';
+
+const MapView: React.FC = () => {
   return (
     <div style={{ width: '100%', height: '500px', background: '#e0e0e0', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <span>Map will be displayed here</span>
     </div>
   );
-}
+};
+
+export default MapView;
