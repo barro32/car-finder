1f844183a2dde954412478aa4f5029f8fedcb376
barro32 <barro32@hotmail.com>
2025-08-12 23:35:08 +0100

    report

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+
 import MapView from '../components/MapView';
+import ReportCarForm from '../components/ReportCarForm';
 
 export default function Home() {
   return (
     <div>
       <h1>Car Finder</h1>
+      <ReportCarForm />
       <MapView />
     </div>
   );
