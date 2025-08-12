f7f4e2de6c04e83ba143c6e0d646d431f27f7544
Dan Barrington <daniel.barrington@healthunlocked.com>
2025-08-12 23:31:13 +0100

    stuff work

--- api/cars.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-// Placeholder for car-related API routes
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'API is working' });
-}
